6edf0c3d98c817d71666ce4e5ac0f007e3f938e4
Saurabh Sharma <sauras4@uci.edu>
2019-05-11 16:21:42 -0700

    server html type

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var index = fs.readFileSync('index.html');
 const port = process.env.PORT || 80;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/html');
   res.end(index);
 });
 server.listen(port, () => {
