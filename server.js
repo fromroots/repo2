dedb912c4119b5be5eb0c60161cb6648878de50e
Saurabh Sharma <sauras4@uci.edu>
2019-05-11 16:20:08 -0700

    try serving an html page

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const http = require('http');
+const fs = require('fs');
+var index = fs.readFileSync('index.html');
 const port = process.env.PORT || 80;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end(index);
 });
 server.listen(port, () => {
   console.log(`Server running at http://${port}/`);
