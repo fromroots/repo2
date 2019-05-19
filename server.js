a57fc5c05651148b654fc590d4ba21451c21c9bb
Saurabh Sharma <sauras4@uci.edu>
2019-05-19 12:46:38 -0700

    Image 1

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 const http = require('http');
+const static = require('node-static');
 const fs = require('fs');
-var index = fs.readFileSync('index.html');
-const port = process.env.PORT || 80;
-const server = http.createServer((req, res) => {
-  // res.statusCode = 200;
-  // res.setHeader('Content-Type', 'text/html');
-  // res.end(index);
-  	req.addListener('end', () -> file.serve(req, res)).resume();
+const port =  process.env.PORT || 8080;;
+
+// const server = http.createServer(function(req, res) {
+//   	req.addListener('end', () function(){
+// 		file.serve(req, res)
+// 	}).resume();
+// });
+var hostname = '127.0.0.1';
+var directory = __dirname + "/."
+var file = new static.Server(directory);
+const server = http.createServer(
+	function(request, response) {
+		console.log(directory);
+		request.addListener('end', function(){
+			console.log("there");
+			file.serve(request, response);
+		}).resume();
+	}
+);
+server.listen(port, hostname, function(){
+  console.log(`Server running at http://${hostname}:${port}`);
 });
-server.listen(port, () => {
-  console.log(`Server running at http://${port}/`);
+
+
+/***********************************************/
+/*       Set up the web socket server         */
+
+var io = require('socket.io').listen(server);
+io.on('connection', function(socket) {
+	console.log("get a connection");
 });
